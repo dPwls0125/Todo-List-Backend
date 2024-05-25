73c8fa3dc5ec828cbf7596ef4dd3c372f4258e9d
shchoilv1 <shawnchoicode@gmail.com>
2024-05-25 15:27:46 +0900

    feat: TodoList

--- src/main/java/com/oop/todo/model/TodoEntity.java
+++ src/main/java/com/oop/todo/model/TodoEntity.java
@@ -1,4 +1,31 @@
 package com.oop.todo.model;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name ="Todo")
 public class TodoEntity {
+    @Id
+    @GeneratedValue(generator="system-uuid") //자동으로 id 생성
+    @GenericGenerator(name="system-uuid",strategy="uuid")
+    private String id;
+    private String userId;
+    private String title;
+    private boolean done;
 }
---
+++ src/main/java/com/oop/todo/service/TodoService.java
@@ -0,0 +1,41 @@
+package com.oop.todo.service;
+
+
+import com.oop.todo.model.TodoEntity;
+import com.oop.todo.persistence.TodoRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@Service
+public class TodoService {
+
+    @Autowired
+    private TodoRepository repository;
+
+    public Optional<TodoEntity> create(final TodoEntity entity){
+        //Validations
+        validate(entity);
+        repository.save(entity);
+        return repository.findById(entity.getId());
+    }
+
+    public List<TodoEntity>retrieve(final String userId) {
+        return repository.findByUserId(userId);
+    }
+
+    public void validate(final TodoEntity entity){
+        if(entity == null) {
+            log.warn("Entity cannot be null.");
+            throw new RuntimeException("Entity cannot be null.");
+        }
+        if(entity.getUserId()==null) {
+            log.warn("Unknown user.");
+            throw new RuntimeException("Unknown user.");
+        }
+    }
+}
