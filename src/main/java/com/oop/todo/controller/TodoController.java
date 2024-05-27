9971fb2f356c4abbf6bb65a350ec6c6574f6a298
shchoilv1 <shawnchoicode@gmail.com>
2024-05-27 09:40:57 +0900

    GET,PUT Update RESST API

--- src/main/java/com/oop/todo/controller/TodoController.java
+++ src/main/java/com/oop/todo/controller/TodoController.java
@@ -76,4 +76,60 @@ public class TodoController {
         // HTTP Status 200 상태로 response를 전송한다.
         return ResponseEntity.ok().body(response);
     }
+
+    @GetMapping("/update")
+    public ResponseEntity<?>update(@RequestBody TodoDTO dto){
+        try {
+            //dto를 이용해 테이블에 저장하기 위한 entity를 생성한다.
+            TodoEntity entity = TodoDTO.toEntity(dto);
+
+            //entity userId를 임시로 지정한다.
+            entity.setUserId("Temporary-user");
+
+            //service.create를 통해 repository에 entity를 저장한다.
+            //이 때 넘어오는 값이 없을 수도 있으므로 List가 아닌 Optional로 한다.
+            Optional<TodoEntity> entities = service.update(entity);
+
+            //entities를 dtos로 스트림 변환한다.
+            List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+
+            //Response DTO를 생선한다.
+            ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
+
+            //HTTP Status 200 상태로 response를 전송한다.
+            return ResponseEntity.ok().body(response);
+        }catch (Exception e) {
+            String error = e.getMessage();
+            ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().error(error).build();
+            return ResponseEntity.badRequest().body(response);
+        }
+    }
+
+    @PutMapping
+    public ResponseEntity<?> updateTodo(@RequestBody TodoDTO dto){
+        try {
+            //dto를 이용해 테이블에 저장하기 위한 entity를 생성한다.
+            TodoEntity entity = TodoDTO.toEntity(dto);
+
+            //entity userId를 임시로 지정한다.
+            entity.setUserId("Temporary-user");
+
+            //service.create를 통해 repository에 entity를 저장한다.
+            //이 때 넘어오는 값이 없을 수도 있으므로 List가 아닌 Optional로 한다.
+            Optional<TodoEntity> entities = service.updateTodo(entity);
+
+            //entities를 dtos로 스트림 변환한다.
+            List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+
+            //Response DTO를 생성한다.
+            ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
+
+            //HTTP Status 200상태로 response를 전송한다.
+            return ResponseEntity.ok().body(response);
+        }catch (Exception e) {
+            String error = e.getMessage();
+            ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().error(error).build();
+            return ResponseEntity.badRequest().body(response);
+        }
+    }
 }
